f451eeca5373b576ab6c4ed838eaf7502b3b8e8e
baadikhaoula <baadikhaoula38@gmail.com>
2026-05-15 19:13:47 +0200

    mise à jour

--- src/main/java/ma/khaoulabaadi/webmvcproducts/WebMvcProductsApplication.java
+++ src/main/java/ma/khaoulabaadi/webmvcproducts/WebMvcProductsApplication.java
@@ -1,7 +1,11 @@
 package ma.khaoulabaadi.webmvcproducts;
 
+import ma.khaoulabaadi.webmvcproducts.entities.Product;
+import ma.khaoulabaadi.webmvcproducts.repository.ProductRepository;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
 
 @SpringBootApplication
 public class WebMvcProductsApplication {
@@ -10,4 +14,33 @@ public class WebMvcProductsApplication {
         SpringApplication.run(WebMvcProductsApplication.class, args);
     }
 
+    @Bean
+    CommandLineRunner commandLineRunner(ProductRepository productRepository) {
+        return args -> {
+            Product product = Product.builder()
+                    .name("Computer")
+                    .price(5400)
+                    .quantity(11)
+                    .build();
+
+            // Correction : suppression de la parenthèse en trop
+            productRepository.save(product);
+
+            productRepository.save(Product.builder()
+                    .name("Printer")
+                    .price(1299)
+                    .quantity(11)
+                    .build());
+
+            productRepository.save(Product.builder()
+                    .name("smart Phone")
+                    .price(12000)
+                    .quantity(33)
+                    .build());
+
+            productRepository.findAll().forEach(p -> {
+                System.out.println(p.toString());
+            });
+        };
+    }
 }
